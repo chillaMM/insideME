38a63eacaee38a638fa318cbfb339ca23bff8bde
chillaMM <36149934+chillaMM@users.noreply.github.com>
2024-07-10 19:48:13 +0000

    f

--- flink-demo-app/flink/src/main/java/summit/KafkaToS3.java
+++ flink-demo-app/flink/src/main/java/summit/KafkaToS3.java
@@ -1,9 +1,12 @@
 package summit;
-
+import java.util.HashSet;
 import org.apache.flink.api.common.serialization.SimpleStringSchema;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.api.functions.sink.filesystem.StreamingFileSink;
-import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer;
+import org.apache.flink.connector.kafka.source.KafkaSource;
+import org.apache.flink.connector.kafka.sink.KafkaSink;
+import org.apache.flink.api.common.eventtime.WatermarkStrategy;
+
 import org.apache.flink.streaming.connectors.kafka.KafkaDeserializationSchema;
 import org.apache.flink.streaming.connectors.kafka.config.StartupMode;
 import org.apache.flink.streaming.connectors.kafka.internals.KafkaDeserializationSchemaWrapper;
@@ -17,28 +20,23 @@ import java.util.concurrent.TimeUnit;
 
 public class KafkaToS3 {
     public static void main(String[] args) throws Exception {
-        final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
 
-        Properties kafkaProperties = new Properties();
-        kafkaProperties.setProperty("bootstrap.servers", "b-1.democluster.v9nt77.c9.kafka.eu-west-1.amazonaws.com:9098,b-2.democluster.v9nt77.c9.kafka.eu-west-1.amazonaws.com:9098");
-        kafkaProperties.setProperty("group.id", "flink-group");
-        kafkaProperties.setProperty("security.protocol", "SASL_SSL");
-        kafkaProperties.setProperty("sasl.mechanism", "AWS_MSK_IAM");
-        kafkaProperties.setProperty("sasl.client.callback.handler.class", "software.amazon.msk.auth.iam.IAMClientCallbackHandler");
-        kafkaProperties.setProperty("sasl.jaas.config", "software.amazon.msk.auth.iam.IAMLoginModule required;");
-        kafkaProperties.setProperty("auto.offset.reset", "earliest");
-        
+      
 
 
+        KafkaSource<String> kafkaConsumer =  KafkaSource.<String>builder()
+        .setTopics("summit-topic")
+        .setBootstrapServers( "b-1.democluster.v9nt77.c9.kafka.eu-west-1.amazonaws.com:9098,b-2.democluster.v9nt77.c9.kafka.eu-west-1.amazonaws.com:9098")
+         .setGroupId( "flink-group")
+         .setProperty("security.protocol", "SASL_SSL")
+        .setProperty("sasl.mechanism", "AWS_MSK_IAM")
+        .setProperty("sasl.client.callback.handler.class", "software.amazon.msk.auth.iam.IAMClientCallbackHandler")
+        .setProperty("sasl.jaas.config", "software.amazon.msk.auth.iam.IAMLoginModule required;")
+        .setProperty("auto.offset.reset", "earliest")
+        .setValueOnlyDeserializer(new SimpleStringSchema()).build();
 
-        FlinkKafkaConsumer<String> kafkaConsumer = new FlinkKafkaConsumer<>(
-                "summit-test-topic",
-                new SimpleStringSchema(),
-                kafkaProperties
-        );
-        kafkaConsumer.setStartFromLatest();
-
-        DataStream<String> kafkaStream = env.addSource(kafkaConsumer);
+        DataStream<String> kafkaStream = env.fromSource(kafkaConsumer, WatermarkStrategy.noWatermarks(),  "Kafka Source" );
 
         final StreamingFileSink<String> sink = StreamingFileSink
                 .forRowFormat(new Path("s3a://dtpl-gds-summit3-processed-messages/test_folder/"), new SimpleStringEncoder<String>("UTF-8"))
